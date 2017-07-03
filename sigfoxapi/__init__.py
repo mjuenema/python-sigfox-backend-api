8ccf17e0d9b1b7a850bfe555707ae51d16842d7e
Markus Juenemann <markus@juenemann.net>
2017-07-03 10:34:47 +1000

    Update __init__.py

--- sigfoxapi/__init__.py
+++ sigfoxapi/__init__.py
@@ -79,6 +79,20 @@ class Sigfox(object):
         """Get the description of a particular group.
 
            :param groupid: The group identifier.
+           
+           >>> s.group_info('489b848ee4b0ca4786945614')
+           {
+               "id":"489b848ee4b0ca4786945614",
+               "name":"Group 1",
+               "nameCI":"group 1",
+               "description":"Group 1 description text",
+               "path":[
+                   "51f13454bc54518c7bae7d4d",
+                   "50f13484b846618c7bae77b7"
+               ],
+               "billable":true,
+               "bssId": "bss-48631656321"
+           }
 
         """
 
@@ -87,6 +101,23 @@ class Sigfox(object):
 
     def group_list(self):
         """Lists all children groups of your group.
+        
+           >>> s.group_list()
+           [
+               {
+                   "id":"510b848ee4b0ca47869752b5",
+                   "name":"Group 1",
+                   "nameCI":"group 1",
+                   "description":"Group 1 description text",
+                   "path":[
+                        "51f13454bc54518c7bae7d4d",
+                        "50f13484b846618c7bae77b7"
+                   ],
+                   "billable":true,
+                   "bssId": "bss-48631656321"
+                   }, 
+                   { ... }
+           ]
 
         """
 
@@ -98,6 +129,18 @@ class Sigfox(object):
 
            :param params: Dictionary of the format described in the
                official documentation
+             
+           >>> params = {
+           ...     "id" : "deadbeef0486300cbebef070",
+           ...     "name" : "dtname",
+           ...     "description" : "the description",
+           ...     "keepAlive" : 3000,
+           ...     "alertEmail" : "alert@email.com",
+           ...     "payloadType" : "None",
+           ...     "downlinkMode" : 0,
+           ...     "downlinkDataString" : "deadbeefcafebabe",
+           ... }
+           >>> s.devicetype_edit(params)
 
         """
 
@@ -107,7 +150,23 @@ class Sigfox(object):
 
     def devicetype_list(self):
         """Lists all device types available to your group.
-
+        
+           ..note:: ```includeSubGroups``` and ```contractInfoId``` are currently not supported.
+        
+           >>> s.devicetype_list()
+           [
+               {
+                   "id" : "4d3091a05ee16b3cc86699ab",
+                   "name" : "Sigfox test device",
+                   "group" : "4d39a4c9e03e6b3c430e2188",
+                   "description" : "Little things in the black boxes",
+                   "keepAlive" : 7200,
+                   "payloadType" : "None",
+                   "contract" : "523b1d10d777d3f5ae038a02"
+               }, 
+               { ... }
+           ]
+    
         """
 
         return self.request('GET', '/devicetypes')
@@ -117,7 +176,31 @@ class Sigfox(object):
         """Get the communication down events for devices belonging to a device type.
 
            :param devicetypeid: The device type identifier.
-
+           
+           >>> s.devicetype_errors('5256c4d6c9a871b80f5a2e50')
+           [ 
+               {
+                   "deviceId" : "0235",
+                   "time" : 1381410600026,
+                   "message" : "No message received since 2013-10-08 15:36:21",
+                   "severity" : "ERROR",
+                   "deviceTypeId" : "5256c4d6c9a871b80f5a2e50",
+                   "callbacks" : [ 
+                       {
+                           "url" : "http://host/path?id=0235&time=1381410600",
+                           "status" : 600,
+                           "info" : "Connection refused: host/path"
+                       }, 
+                       {
+                           "subject" : "some subject",
+                           "message" : "some messages",
+                           "status" : 200
+                       }
+                   ]
+               }, 
+               { ... }
+           ]
+           
         """
 
         return self.request('GET', '/devicetypes/%s/status/error' % (devicetypeid))
@@ -128,6 +211,8 @@ class Sigfox(object):
            belonging to a device type.
 
            :param devicetypeid: The device type identifier.
+           
+           See `Sigfox.errors()` for example output.
 
         """
 
@@ -141,6 +226,26 @@ class Sigfox(object):
         """Get the messages that were sent by all the devices of a device type.
 
            :param devicetypeid: The device type identifier.
+           
+           >>> s.devicetype_messages('5256c4d6c9a871b80f5a2e50')
+           [
+               {
+                   "device" : "002C",
+                   "time" : 1343321977,
+                   "data" : "3235353843fc",
+                   "snr" : "38.2",
+                   "computedLocation": {
+                     "lat" : 43.45,
+                     "lng" : 6.54,
+                     "radius": 500
+                   },
+                   "linkQuality" : "GOOD",
+                   "downlinkAnswerStatus" : {
+                       "data" : "1511000a00007894"
+                    } 
+               },
+               { ... }
+           ]
 
         """
 
@@ -152,6 +257,9 @@ class Sigfox(object):
            of the device type.
 
            :param devicetypeid: The device type identifier.
+           
+           >>> s.devicetype_disengage('5256c4d6c9a871b80f5a2e50')
+           None
 
         """
         return self.request('GET', '/devicetypes/%s/disengage' % (devicetypeid))
@@ -160,9 +268,38 @@ class Sigfox(object):
     def callback_new(self, devicetypeid, callbacks):
         """Create new callbacks.
 
-
            :param callbacks: List of dictionaries as described in the official
                documentation.
+               
+           >>> new_callbacks = [
+           ...     {
+           ...         "channel" : "URL",
+           ...         "callbackType" : 0,
+           ...         "callbackSubtype" : 2,
+           ...         "url" : "http://myserver.com/sigfox/callback",
+           ...         "httpMethod" : "POST",
+           ...         "enabled" : true,
+           ...         "sendDuplicate" : false,
+           ...         "sendSni": false,
+           ...         "payloadConfig" : "var1::bool:1",
+           ...         "bodyTemplate" : "device : {device} / {customData#var1}",
+           ...         "headers" : {
+           ...          "time" : "{time}"
+           ...         },
+           ...         "contentType" : "text/plain"
+           ...     }, 
+           ...     {
+           ...         "channel" : "BATCH_URL",
+           ...         "callbackType" : 0,
+           ...         "callbackSubtype" : 2,
+           ...         "url" : "http://myserver.com/sigfox/callback/batch",
+           ...         "linePattern" : "{device};{data};",
+           ...         "enabled" : true,
+           ...         "sendDuplicate" : false,
+           ...         "sendSni": false
+           ...     }
+           ... ]
+           >>> s.callback_new('5256c4d6c9a871b80f5a2e50', new_callbacks)
 
         """
 
@@ -174,6 +311,26 @@ class Sigfox(object):
         """List the callbacks for a device type.
 
            :param devicetypeid: The device type identifier.
+           
+           >>> s.callback_list('5256c4d6c9a871b80f5a2e50')
+           [
+               {
+                   "id" : "deadbeeffacecafebabecafe"
+                   "channel" : "URL",
+                   "callbackType" : 0,
+                   "payloadConfig" : "int1::uint:8 int2::uint:8",
+                   "callbackSubtype" : 0,
+                   "urlPattern" : "http://myserver.com/sigfox/callback",
+                   "httpMethod" : "POST",
+                   "headers" : { "key1" : "value1",
+                                 "key2" : "value2" },
+                   "enabled" : true,
+                   "sendDuplicate" : false,
+                   "dead":false,
+                   "downlinkHook":false
+               },
+               { ... }
+           ]
 
         """
 
@@ -185,6 +342,8 @@ class Sigfox(object):
 
            :param devicetypeid: The device type identifier.
            :param callbackid: The callback identifier.
+           
+           >>> s.callback_delete('5256c4d6c9a871b80f5a2e50', 'deadbeeffacecafebabecafe')
 
         """
 
@@ -197,6 +356,8 @@ class Sigfox(object):
 
            :param devicetypeid: The device type identifier.
            :param callbackid: The callback identifier.
+           
+           >>> s.callback_enable('5256c4d6c9a871b80f5a2e50', 'deadbeeffacecafebabecafe')
 
         """
 
@@ -209,6 +370,8 @@ class Sigfox(object):
 
            :param devicetypeid: The device type identifier.
            :param callbackid: The callback identifier.
+           
+           >>> s.callback_disable('5256c4d6c9a871b80f5a2e50', 'deadbeeffacecafebabecafe')
 
         """
 
@@ -221,6 +384,8 @@ class Sigfox(object):
 
            :param devicetypeid: The device type identifier.
            :param callbackid: The callback identifier.
+           
+           >>> s.callback_downlink('5256c4d6c9a871b80f5a2e50', 'deadbeeffacecafebabecafe')
 
         """
 
